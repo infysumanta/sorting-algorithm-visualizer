7d0cecdc9cf52c8077091d4afc0c5b4859c3d34a
Sumanta <sumantablog@gmail.com>
2024-04-01 15:40:35 +0530

    Add sorting algorithm data to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { sortingAlgorithmsData } from "@/lib/constant";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      {Object.entries(sortingAlgorithmsData).map(([key, value]) => {
+        return (
+          <div key={key}>
+            <h2>{value.title}</h2>
+            <p>{value.description}</p>
+            <p>Worst Case: {value.worstCase}</p>
+            <p>Average Case: {value.averageCase}</p>
+            <p>Best Case: {value.bestCase}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
