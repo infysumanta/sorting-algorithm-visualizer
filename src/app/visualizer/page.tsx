39788db701c6bd34b68bb9535de63364cf3cc105
Sumanta <sumantablog@gmail.com>
2024-04-02 12:40:53 +0530

    Add experimental flag to next.config.mjs and remove fallback prop from Suspense component in page.tsx

--- src/app/visualizer/page.tsx
+++ src/app/visualizer/page.tsx
@@ -4,7 +4,7 @@ import { Suspense } from "react";
 export default function VisualizerPage() {
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <VisualzerComponent />
       </Suspense>
     </>
